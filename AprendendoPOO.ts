116a2a4705f8887200f83b0eab9d979bd1af5e8d
LeandroSampaio001 <leosampaio1990@gmail.com>
2025-09-10 16:38:28 -0300

    Create AprendendoPOO.ts
    aula video

---
+++ AprendendoPOO.ts
@@ -0,0 +1,13 @@
+class Computador{
+    nome:string="Computadaor 1";
+    ram:number=0;
+    cpu:number=0;
+    ligado:boolean=false;
+    
+}
+//instanciar criar o objeto da classe
+const comp1=new Computador();
+const comp2=new Computador();
+const comp3=new Computador();
+
+console.log(comp1.nome);
